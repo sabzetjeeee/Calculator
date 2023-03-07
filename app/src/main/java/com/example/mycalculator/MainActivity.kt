4ad94c67b84579178ffd55773d057026dcf931e3
sabzetjeeee <74899093+sabzetjeeee@users.noreply.github.com>
2023-03-07 22:15:11 +0200

    Pозрахунок площi прямокутника

--- app/src/main/java/com/example/mycalculator/MainActivity.kt
+++ app/src/main/java/com/example/mycalculator/MainActivity.kt
@@ -14,6 +14,10 @@ class MainActivity : AppCompatActivity() {
     private var mainbttn2: Button? = null
     private var inputText2: EditText? = null
     private var viewResult2: TextView? = null
+    private var mainbttn3: Button? = null
+    private var inputText3: EditText? = null
+    private var inputText4: EditText? = null
+    private var viewResult3: TextView? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,6 +29,11 @@ class MainActivity : AppCompatActivity() {
         mainbttn2 = findViewById(R.id.bttn2)
         inputText2 = findViewById(R.id.etInput2)
         viewResult2 = findViewById(R.id.tvResult2)
+
+        mainbttn3 = findViewById(R.id.bttn3)
+        inputText3 = findViewById(R.id.etInput3)
+        inputText4 = findViewById(R.id.etInput4)
+        viewResult3 = findViewById(R.id.tvResult3)
 
         mainbttn1?.setOnClickListener {
             val R0 = inputText1?.text.toString()
@@ -38,6 +47,15 @@ class MainActivity : AppCompatActivity() {
             val a = a0.toDouble()
             val S = a * a
             viewResult2?.text = "S=$S"
+        }
+
+        mainbttn3?.setOnClickListener {
+            val a0 = inputText3?.text.toString()
+            val b0 = inputText4?.text.toString()
+            val a = a0.toDouble()
+            val b = b0.toDouble()
+            val S = a * b
+            viewResult3?.text = "S=$S"
         }
     }
 }

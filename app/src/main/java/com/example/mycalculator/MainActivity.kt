549f23f94ff43a9d496ec7d2292df4700229eb6b
sabzetjeeee <74899093+sabzetjeeee@users.noreply.github.com>
2023-03-07 21:22:00 +0200

    Розрахунок площі квадрата

--- app/src/main/java/com/example/mycalculator/MainActivity.kt
+++ app/src/main/java/com/example/mycalculator/MainActivity.kt
@@ -11,7 +11,9 @@ class MainActivity : AppCompatActivity() {
     private var inputText1: EditText? = null
     private var viewResult1: TextView? = null
     private var pi = 3.14
-
+    private var mainbttn2: Button? = null
+    private var inputText2: EditText? = null
+    private var viewResult2: TextView? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,11 +21,23 @@ class MainActivity : AppCompatActivity() {
         mainbttn1 = findViewById(R.id.bttn1)
         inputText1 = findViewById(R.id.etInput1)
         viewResult1 = findViewById(R.id.tvResult1)
+
+        mainbttn2 = findViewById(R.id.bttn2)
+        inputText2 = findViewById(R.id.etInput2)
+        viewResult2 = findViewById(R.id.tvResult2)
+
         mainbttn1?.setOnClickListener {
             val R0 = inputText1?.text.toString()
             val R = R0.toDouble()
             val S = R * R * pi
             viewResult1?.text = "S=$S"
+        }
+
+        mainbttn2?.setOnClickListener {
+            val a0 = inputText2?.text.toString()
+            val a = a0.toDouble()
+            val S = a * a
+            viewResult2?.text = "S=$S"
         }
     }
 }
